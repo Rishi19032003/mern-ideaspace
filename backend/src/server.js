b03ca9209581ea3a1de6a22845d4f0952624889f
Rishi19032003 <bvrishi9@gmail.com>
2025-08-29 13:50:45 +0530

    Added the rateLimiter middleware and modififed the connection establishment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import router from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 // const express = require("express")
@@ -9,13 +10,27 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5001
 
-connectDB();
-
 //middleware
-app.use(express.json())
+app.use(express.json()) //this middleware will parse the JSON bodies: req.body
+
+// Implementing a simple middlware funtionality
+// app.use((req, res, next) => {
+//     console.log(`Request method is ${req.method} & Req URL is ${req.url}`)
+//     next()
+// })
+
+app.use(rateLimiter)
 
 app.use("/api/notes", router)
 
-app.listen(PORT, () => {
-    console.log("Server listening on the port ", PORT)
-})
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Server listening on the port", PORT)
+        })
+    })
+
+    .catch(err => {
+        console.error("Failed to connect DB:", err)
+        process.exit(1)
+    })
