b021ab89c5fde6380e1ee58a4e696e5eaf24f8b3
Rishi19032003 <bvrishi9@gmail.com>
2025-09-01 19:33:21 +0530

    Note creation page layout

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import { ArrowLeftIcon } from "lucide-react"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { Link } from "react-router"
 
 const CreatePage = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handelSubmit = async (e) => {
+    if (!title.trim() || !content.trim()) {
+      e.preventDefault()
+      toast.error("All fields are required")
+      return
+    }
+
+    setLoading(true)
+
+    try {
+      await axios.post("http://")
+    } catch (error) {
+      
+    } finally {
+      
+    }
+  }
+
   return (
-    <div>CreatePage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            To Home
+          </Link>
+
+          <div className="card bg-[#201d1d]">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handelSubmit}>
+                {/* Title of the Note */}
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input type="text" placeholder="Enter note title" className="input input-bordered" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
+
+                {/* Content of the Note */}
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <textarea placeholder="Write your note here..." className="textarea textarea-bordered h-32" value={content} onChange={(e) => setContent(e.target.value)} />
+                </div>
+
+                {/* Making of the create button */}
+                <div className="card-actions justify-end">
+                  <button type="submit" className="btn btn-primary" disabled={loading}>
+                    { loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
