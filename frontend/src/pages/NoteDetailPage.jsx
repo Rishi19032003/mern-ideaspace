a4d584a1a42d47f7ac3a4d39c6511483280a172c
Rishi19032003 <bvrishi9@gmail.com>
2025-09-02 22:30:07 +0530

    Updating and viewing the note cards

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from "react"
+import { useNavigate, useParams } from "react-router"
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState("")
+  const [loading, setLoading] = useState(true)
+  const [saving, setSaving] = useState(false)
+
+  const navigate = useNavigate()
+
+  const { id } = useParams()
+
+  console.log({id})
+
   return (
-    <div>NoteDetailPage</div>
+    <div></div>
   )
 }
 
