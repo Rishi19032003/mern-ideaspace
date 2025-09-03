39ba8ce73cfb3bedefb17738ba137450fedc9f2f
Rishi19032003 <bvrishi9@gmail.com>
2025-09-03 20:19:31 +0530

    Completed with the updation funtionality of the application

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
-import { useNavigate, useParams } from "react-router"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router"
+import api from "../lib/axios"
+import { ArrowLeftIcon, LoaderIcon, TrashIcon } from "lucide-react"
+import toast from "react-hot-toast"
 
 const NoteDetailPage = () => {
   const [note, setNote] = useState("")
@@ -8,12 +11,110 @@ const NoteDetailPage = () => {
 
   const navigate = useNavigate()
 
-  const { id } = useParams()
+  const { id } = useParams()//This should be same as it is mentioned in the App.jsx since it is destructuring the id value
 
-  console.log({id})
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`notes/${id}`)
+        setNote(res.data)
+      } catch (error) {
+        console.log("Error in NoteDetailPage function")
+        toast.error("Not able to get the notes")
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchNote()
+  }, [id])
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this note?")) return
+
+    try {
+      await api.delete(`/notes/${id}`)
+      toast.success("Note deleted successfully!")
+      navigate("/")
+    } catch (error) {
+      toast.error("Failed to delete the Note")
+      console.error("error in the handledelete function", error)
+    }
+  }
+
+  const handleSave = async () => {
+    if (!note.title.trim() || !note.content.trim()) {
+      toast.error("All the fields are required!!")
+      return
+    }
+
+    setSaving(true)
+
+    try {
+      await api.put(`/notes/${id}`, note)
+      toast.success("Note updated successfully!")
+      navigate("/")
+    } catch (error) {
+      toast.error("Failed to update the Note")
+      console.error("error in the handleSave function", error)
+    } finally {
+      setSaving(false)
+    }
+
+  }
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+        Loading
+      </div>
+    )
+  }
 
   return (
-    <div></div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to={"/"} className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to Home
+            </Link>
+            <button onClick={handleDelete} className="btn btn-error btn-outline">
+              <TrashIcon className="h-5 w-5" />
+              Delete Note
+            </button>
+          </div>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              {/* Title Updating layout */}
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input type="text" placeholder="Note title" className="input input-bordered" value={note.title} onChange={(e) => setNote({ ...note, title: e.target.value })} />
+              </div>
+              {/* Content Updating layout */}
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea placeholder="Write your note here" className="textarea textarea-bordered h-32" value={note.content} onChange={(e) => setNote({ ...note, content: e.target.value })} />
+              </div>
+
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" disabled={saving} onClick={handleSave}>
+                  {saving ? "Saving..." : "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
